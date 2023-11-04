551e106da5c9ac38d1fd680594e8d5fe04cf731b
Kyle Pedersen <kylepedersen@berkeley.edu>
2023-11-03 17:49:50 -0700

    add main loop

---
+++ build/CMakeFiles/RunLinkedListTests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RunLinkedListTests.dir/tests/test_linkedlist.cpp.o"
+  "RunLinkedListTests.pdb"
+  "RunLinkedListTests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RunLinkedListTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
